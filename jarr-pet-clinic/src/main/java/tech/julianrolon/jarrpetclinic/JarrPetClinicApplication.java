09f29ccc67d75640bda49a6eae33b322371ad02b
julk <julk@192.168.0.15>
2019-05-10 17:19:11 -0500

    First Commit

---
+++ jarr-pet-clinic/src/main/java/tech/julianrolon/jarrpetclinic/JarrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package tech.julianrolon.jarrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarrPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarrPetClinicApplication.class, args);
+	}
+
+}
